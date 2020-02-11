10eeb2dbc6ba7a15005408c785073ac5560b2289
Kyle Geske <stungeye@gmail.com>
2020-02-11 15:55:07 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ToDo-Vue
-Created with CodeSandbox
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-three-fifths is-offset-one-fifth">
+          <header>
+            <h1 class="title">Getting Things Done!</h1>
+          </header>
+          <button class="button">This</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import "./assets/sass/bulma-import.scss";
+
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+header {
+  text-align: center;
+}
+</style>
